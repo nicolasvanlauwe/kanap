5cb7876d30711cc669982748973f86c7c7d0b645
Nicolas Vanlauwe <nicolas/vanlauwe@hotmail.com>
2023-01-06 10:28:53 +0100

    Affichage des canapés dans l'index

--- front/js/script.js
+++ front/js/script.js
@@ -1,6 +1,20 @@
 fetch('http://localhost:3000/api/products')
     .then(response => response.json())
-
     .then(data => {
-        console.log(data);
+        allProducts(data);
     })
+
+
+function allProducts(data) {
+    for (data of data) {
+        let display = document.querySelector('#items');
+        display.innerHTML +=
+            `<a href="./product.html?id=${data._id}">
+        <article>
+        <img src="${data.imageUrl}" alt="${data.altTxt}">
+        <h3 class="productName">${data.name}</h3>
+        <p class="productDescription">${data.description}</p>
+        </article>
+        </a>`;
+    }
+}
